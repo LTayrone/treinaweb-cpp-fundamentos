88e4fbfedca8a20615d529d0b73f47c679f482a4
Cleyson <cleysonph@gmail.com>
2024-06-16 16:37:15 -0300

    01:05 - Entendo a estrutura do código

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+// Definição do namespace std
+using namespace std;
+
+// Função principal
 int main(int argc, char const *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    /*
+        Qualquer coisa
+        que será ignorada
+        pelo compilador
+    */
+    cout << "Hello, World!" << endl;
     return 0;
 }
