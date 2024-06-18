8f7268a7744dd444b6c2915c9f2ee0f03cddab87
Cleyson <cleysonph@gmail.com>
2024-06-17 22:07:40 -0300

    03:11 - Operadores relacionais

---
+++ operadores/relacionais.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a = 5;
+    int b = 10;
+
+    // Igual ==
+    cout << (a == b) << endl;
+
+    // Diferente !=
+    cout << (a != b) << endl;
+
+    // Maior que >
+    cout << (a > b) << endl;
+
+    // Menor que <
+    cout << (a < b) << endl;
+
+    // Maior ou igual >=
+    cout << (a >= b) << endl;
+
+    // Menor ou igual <=
+    cout << (a <= b) << endl;
+
+    return 0;
+}
