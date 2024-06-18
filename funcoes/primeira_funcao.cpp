82f5dcb8e3f96d0b0e1212ed9abc5085cf912cb0
Cleyson <cleysonph@gmail.com>
2024-06-18 01:51:41 -0300

    07:21 - Criando a primeira função

---
+++ funcoes/primeira_funcao.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void greeting()
+{
+    cout << "Olá, Mundo" << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    greeting();
+    greeting();
+    greeting();
+    greeting();
+    greeting();
+
+    return 0;
+}
